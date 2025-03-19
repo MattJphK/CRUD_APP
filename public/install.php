3e38fbcddc42b7fd3b65841d2644ba245e35de4f
MattJphK <b00160049@mytudublin.ie>
2025-03-19 17:55:47 +0000

    crud

--- public/install.php
+++ public/install.php
@@ -8,11 +8,21 @@
  */
 
 require "config.php";
-
+//the require statement isn't working so I'm adding it in directly
+$host = "localhost";
+$username = "root";
+$password = "Ilovetea24!";
+$dbname = "test";
+$dsn = "mysql:host=$host;dbname=$dbname"; // will use later
+$options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+);
+$sql = "";
 try {
+
     $connection = new PDO("mysql:host=$host;dbname=$dbname", $username,
         $password,$options);
-    $sql = file_get_contents("data/init.sql");
+    $sql = file_get_contents("init.sql");
     $connection->exec($sql);
 
     echo "Database and table users created successfully";
