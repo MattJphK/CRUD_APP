3e4aa6871f6201390699f78a2549e1449e3c9396
MattJphK <b00160049@mytudublin.ie>
2025-03-19 01:00:04 +0000

    page 9

---
+++ public/read.php
@@ -0,0 +1,15 @@
+
+<?php include "templates/header.php"; ?>
+
+<h2>Find user based on location</h2>
+
+<form method="POST"></form>
+    <label for="location">Location</label>
+    <input type="text" id="location" name="location" placeholder="Enter location">
+    <input type="submit" value="View Results">
+</form>
+
+<a href="index.php">Back to home</a>
+
+
+<?php include "templates/footer.php"; ?>
