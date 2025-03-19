ee420db5591616c99f073152e3c64ed3d4235b32
MattJphK <b00160049@mytudublin.ie>
2025-03-19 00:30:00 +0000

    page 5

--- public/templates/header.php
+++ public/templates/header.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Simple Database App</title>
+
+        <link rel='stylesheet' type='text/css' href='css/style.css' />
+    </head>
+<body>
+    <h1>Simple Database App</h1>
+
